57ab03392a7fca47680f026537e0fd16c52f3f56
LangaMAd <artykovh@gmail.com>
2023-06-27 19:32:47 +0600

    update urls

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -8,5 +8,12 @@ urlpatterns = [
     path('', IndexPage.as_view(), name='index'),
     path('login/',LoginView.as_view(), name='login'),
     path('register/',UserRegisterView.as_view(), name = 'register'),
-    path('logout/',UserLogout,name = 'logout')
+    path('logout/',UserLogout,name = 'logout'),
+    path('students/', StudentListView.as_view(), name='teacher_room'),
+    path('students/create/', StudentCreateView.as_view(), name='create_student'),
+    path('students/update/<int:pk>/', StudentUpdateView.as_view(), name='update_student'),
+    path('students/delete/<int:pk>/', StudentDeleteView.as_view(), name='delete_student'),
+    path('students/view/<int:pk>/', StudentDetailView.as_view(), name='view_student'),
+    path('send_email/', SendEmailView.as_view(), name='send_email')
+
 ]
