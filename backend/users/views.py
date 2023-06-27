5c857222382e5c64e6a3d4d3699ccea10349e2b1
LangaMAd <artykovh@gmail.com>
2023-06-27 18:36:43 +0600

    add CRUD (views)

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render, redirect
+
 from django.views.generic import (
     FormView,
     CreateView,
-    TemplateView)
+    TemplateView,
+    UpdateView,
+    DeleteView,
+    ListView,)
 from django.contrib.auth import login,authenticate,logout
 from  django.http import HttpResponse
 from django.urls import reverse_lazy
 from .form import LoginForm, UserRegisterForm
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Student
+
 # Create your views here.
 
 
@@ -22,9 +28,10 @@ class LoginView(FormView):
 
     def form_valid(self, form):
         data = form.cleaned_data
+        username = data['username']
         phone = data['phone']
         password = data['password']
-        user = authenticate(phone=phone, password=password)
+        user = authenticate(username=username , phone=phone, password=password)
         if user is not None:
             if user.is_active:
                 login(self.request, user)
@@ -36,7 +43,7 @@ class LoginView(FormView):
 class UserRegisterView(CreateView):
     template_name = 'register.html'
     form_class = UserRegisterForm
-    success_url = reverse_lazy('teacher_room')
+    success_url = reverse_lazy('index')
 
 
 
@@ -44,5 +51,41 @@ def UserLogout(request):
     if request.user.is_authenticated:
         logout(request)
         return redirect('index')
+
+
+
+class StudentListView(ListView):
+    model = Student
+    template_name = 'teacher_room.html'
+    context_object_name = 'students'
+
+
+class StudentCreateView(CreateView):
+    model = Student
+    template_name = 'create_student.html'
+    fields = ['name', 'email', 'birth_date', 'grade', 'address', 'gender']
+    success_url = '/students/'
+
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    template_name = 'update_student.html'
+    fields = ['name', 'email', 'birth_date', 'grade', 'address', 'gender']
+    success_url = '/students/'
+
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    template_name = 'delete_student.html'
+    success_url = '/students/'
+
+
+class StudentDetailView(DeleteView):
+    model = Student
+    template_name = 'view_student.html'
+    context_object_name = 'student'
+
+
+
 
 
